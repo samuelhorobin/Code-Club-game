bbe648bcfcda45c56bf8894e621a2199bde4cb36
Sam <Samuelhorobin461@gmail.com>
2023-03-29 16:26:16 +0100

    main

---
+++ roverhills v1.0/main.py
@@ -0,0 +1,14 @@
+''' LESSON 1: Drawing an object'''
+# Libraries
+import pygame
+
+# Constants
+RESOLUTION = (1920, 1080)
+
+def main():
+    pygame.init()
+    screen = pygame.display(RESOLUTION)
+
+if __name__ == "__main__":
+    main()
+
